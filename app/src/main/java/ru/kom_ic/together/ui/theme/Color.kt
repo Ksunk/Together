90f6779334e406bacba05ad13c7c3df1cbf29c21
Kmiha <kmiha@iqbuzz.ru>
2022-06-02 18:07:20 +0300

    Start project

---
+++ app/src/main/java/ru/kom_ic/together/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package ru.kom_ic.together.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LightBlue = Color(0xFFDAEDE7)
+val Blue = Color(0xFFB1DCD2)
+val MediumBlue = Color(0xFF74A29E)
+val DarkBlue = Color(0xFF4D6568)
+val Pink = Color(0xFFF0D3C2)
+val DarkPink = Color(0xFFC16471)
